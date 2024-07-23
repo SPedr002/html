2507e5ebc169a7caf2d04e2a3164c833f4eb10fe
SPedr002 <pgbf@academico.ufpb.br>
2024-07-23 19:37:44 -0300

    Add files via upload

---
+++ portifólio/app.js
@@ -0,0 +1,9 @@
+function changeColor1(){
+    document.getElementById('themeStyle').setAttribute('href','style.css');
+    
+}
+
+function changeColor2(){
+    document.getElementById('themeStyle').setAttribute('href','style2.css');
+    
+}
