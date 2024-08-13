257d1a7927f0e1beeba17acaa60022a29c458ffb
SPedr002 <pgbf@academico.ufpb.br>
2024-08-13 18:41:50 -0300

    Delete portifólio directory

--- portifólio/app.js
+++
@@ -1,9 +0,0 @@
-function changeColor1(){
-    document.getElementById('themeStyle').setAttribute('href','style.css');
-    
-}
-
-function changeColor2(){
-    document.getElementById('themeStyle').setAttribute('href','style2.css');
-    
-}
