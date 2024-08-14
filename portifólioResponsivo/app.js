ba006914174003b9a4baf52c7fc1b7b44ec59368
SPedr002 <pgbf@academico.ufpb.br>
2024-08-14 12:57:37 -0300

    Add files via upload

---
+++ portifólioResponsivo/app.js
@@ -0,0 +1,15 @@
+function changeColor1(){
+    document.getElementById('themeStyle').setAttribute('href','./styles/style.css');
+    document.getElementById('instagram').setAttribute('src','./assets/instagramVermelho.png');
+    document.getElementById('github').setAttribute('src','./assets/githubVermelho.png');
+    document.getElementById('linkedin').setAttribute('src','./assets/linkedinVermelho.png');
+
+}
+
+function changeColor2(){
+    document.getElementById('themeStyle').setAttribute('href','./styles/style2.css');
+    document.getElementById('instagram').setAttribute('src','./assets/instagramAzul.png');
+    document.getElementById('github').setAttribute('src','./assets/githubAzul.png');
+    document.getElementById('linkedin').setAttribute('src','./assets/linkedinAzul.png');
+
+}
